a6c4f69443d035c62436fe006526ea070f37fa5e
vannidelprete <pibe11@hotmail.it>
2020-02-27 16:53:38 +0100

    Data Preprocessing UP

---
+++ DataPreprocessing.py
@@ -0,0 +1,12 @@
+#============= IMPORT ==========================
+import pandas as pd
+import numpy as np
+
+brain_tumor_data = pd.read_csv(r"Data/bt_dataset_t3.csv")
+
+#============= FIX MISSING DATA =================
+brain_tumor_data = brain_tumor_data.replace(np.inf, 999)
+bt_rep = brain_tumor_data.mean()
+brain_tumor_data = brain_tumor_data.fillna(bt_rep)
+
+brain_tumor_data.to_csv(r"Data/bt_dataset_t3_fixed.csv")
